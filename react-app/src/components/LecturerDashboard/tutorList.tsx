033f30382eac31509efe06fbba70bf6aa95c6c5b
malithrukshankg75 <S4042050@student.rmit.edu.au>
2025-04-03 20:34:16 +1100

    Feature: Tutor list component added

--- react-app/src/components/LecturerDashboard/tutorList.tsx
+++ react-app/src/components/LecturerDashboard/tutorList.tsx
@@ -1,3 +1,50 @@
-export const tutorList = () => {
-  return <></>;
+import React, { useState ,useEffect} from "react";
+import { Tutor } from "../../types/Tutor";
+import TutorCard from "./tutorCard";
+
+interface Props {
+    tutors: Tutor[];
+}
+
+const TutorList: React.FC<Props> = ({ tutors }) => {
+
+    const [localTutors, setLocaltutors] = useState<Tutor[]>(tutors);
+
+    
+
+    const handleUpdate = (updatedApplicant: Tutor) => {
+        const updatedList = localTutors.map((tutor) =>
+            tutor.id === updatedApplicant.id ? updatedApplicant : tutor
+        );
+        setLocaltutors(updatedList);
+       
+        console.log("Tutors")
+        console.log(tutors)
+      };
+    
+      useEffect(() => {
+        console.log("📦 Tutors updated in TutorList:", tutors);
+        setLocaltutors(tutors);
+      }, [tutors]);
+
+
+if (localTutors.length === 0) {
+    return <p className="text-gray-500 text-center mt-6">No applicants found.</p>;
+  }
+
+  
+  
+  return (
+    <div className="applicant-list">
+      {localTutors.map((tutor) => (
+        <TutorCard
+          key={tutor.id}
+          tutor={tutor}
+          onUpdate={handleUpdate}
+        />
+      ))}
+    </div>
+  );
 };
+
+export default TutorList;
